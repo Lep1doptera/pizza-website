ef86bc3e6691708e4d3fecb5e0514c506b76ce07
Mandy <ferguson.am19@gmail.com>
2025-09-17 21:21:52 +1000

    Started Menu page for Product Card

---
+++ app/components/product-card.js
@@ -0,0 +1,9 @@
+export default function ProductCard() {
+    return (
+        <div className="bg-white rounded-lg shadow-md overflow-hidden w-64 p-4">
+            <h2>Card Title Test</h2>
+            <p>Card Description asdfasdfadfasdf</p>
+            <p>Price</p>
+        </div>
+    )
+}
--- app/menu/page.js
+++ app/menu/page.js
@@ -1,5 +1,16 @@
 // Menu page
+import ProductCard from '../components/product-card'
 
 export default function MenuPage() {
-  return <h1>Test for Menu</h1>
+  return (
+    <div>
+      <div className="relative w-full h-40 flex items-center justify-center text-white text-2xl font-bold bg-blue-900">
+        Mains
+      </div>
+
+      <div>
+        <ProductCard />
+      </div>
+    </div>
+  );
 }
