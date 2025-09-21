b20bf1de34e798c332537747358512396a82df5b
Mandy <ferguson.am19@gmail.com>
2025-09-21 19:18:02 +1000

    Products added with details

--- app/components/product-card.js
+++ app/components/product-card.js
@@ -1,4 +1,5 @@
 export default function ProductCard({ name, description, cost, sizes, image }) {
+  console.log(name, cost, sizes);
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden w-full max-w-md p-6">
       <img
@@ -6,12 +7,15 @@ export default function ProductCard({ name, description, cost, sizes, image }) {
         alt={name}
         className="w-full h-40 object-cover rounded-md mb-3"
       />
-      <h2>{name}</h2>
+      <h2 className="font-bold">{name}</h2>
       <p>{description}</p>
       {sizes ? (
         <div>
-          <p>Size:{JSON.stringify(sizes)}</p>
-          <p>${cost}</p>
+          {sizes.map((option, index) => (
+            <p key={index}>
+              {option.size} ${option.cost.toFixed(2)}
+            </p>
+          ))}
         </div>
       ) : (
         <div>
--- app/menu/page.js
+++ app/menu/page.js
@@ -4,7 +4,9 @@ import ProductCard from "../components/product-card";
 export default function MenuPage() {
   const mains = menu.find((category) => category.category === "mains");
   const pizza = menu.find((category) => category.category === "pizza");
-
+  const dessert = menu.find((category) => category.category === "dessert");
+  const sides = menu.find((category) => category.category === "sides");
+  const deals = menu.find((category) => category.category === "specials");
   return (
     <div>
       <div className="relative w-full h-30 flex items-center justify-center text-white text-2xl font-bold bg-blue-900">
@@ -28,9 +30,29 @@ export default function MenuPage() {
       <div className="relative w-full h-30 flex items-center justify-center text-white text-2xl font-bold bg-blue-900">
         Pizza
       </div>
+
       <div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6">
           {pizza.items.map((item, index) => (
+            <ProductCard
+              key={index}
+              name={item.name}
+              description={item.description}
+              sizes={item.sizes}
+              cost={item.cost}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="relative w-full h-30 flex items-center justify-center text-white text-2xl font-bold bg-blue-900">
+        Desserts
+      </div>
+
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6">
+          {dessert.items.map((item, index) => (
             <ProductCard
               key={index}
               name={item.name}
@@ -42,6 +64,41 @@ export default function MenuPage() {
         </div>
       </div>
 
+      <div className="relative w-full h-30 flex items-center justify-center text-white text-2xl font-bold bg-blue-900">
+        Sides
+      </div>
+
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6">
+          {sides.items.map((item, index) => (
+            <ProductCard
+              key={index}
+              name={item.name}
+              description={item.description}
+              cost={item.cost}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="relative w-full h-30 flex items-center justify-center text-white text-2xl font-bold bg-blue-900">
+        Deals
+      </div>
+
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6">
+          {deals.items.map((item, index) => (
+            <ProductCard
+              key={index}
+              name={item.name}
+              description={item.description}
+              cost={item.cost}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
