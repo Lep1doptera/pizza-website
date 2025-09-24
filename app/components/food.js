043a17e1147ded7e517c942686e04584d3c4f2c5
Mandy <ferguson.am19@gmail.com>
2025-09-24 18:42:03 +1000

    changed add label and costs for removal

--- app/components/food.js
+++ app/components/food.js
@@ -9,9 +9,9 @@ const menu = [
         cost: 16.5,
         image: "/images/pasta.jpg",
         additions: [
-          {add: "Parmesan", cost: 4},
-          {add: "Ragu", cost: 5}
-        ]
+          { name: "Parmesan", cost: 4 },
+          { name: "Ragu", cost: 5 },
+        ],
       },
       {
         name: "Spaghetti Bolognese",
@@ -20,12 +20,12 @@ const menu = [
         cost: 15,
         image: "/images/pasta.jpg",
         additions: [
-          {add: "Parmesan", cost: 4},
-          {add: "Penne", cost: 2},
-          {add: "Gnocci", cost: 2},
-          {add: "sauce", cost: 5},
-          {add: "sauce", cost: 2}
-        ]
+          { name: "Parmesan", cost: 4 },
+          { name: "Penne", cost: 2 },
+          { name: "Gnocci", cost: 2 },
+          { name: "sauce", cost: 5 },
+          { name: "sauce", cost: 2 },
+        ],
       },
       {
         name: "Chicken Parmigiana",
@@ -34,15 +34,15 @@ const menu = [
         cost: 18,
         image: "/images/pasta.jpg",
         additions: [
-          {add: "Mozzarella", cost: 2},
-          {add: "Ham", cost: 3},
-          {add: "Napoli", cost: 2}
+          { name: "Mozzarella", cost: 2 },
+          { name: "Ham", cost: 3 },
+          { name: "Napoli", cost: 2 },
         ],
         remove: [
-          {withdraw: "Mozzarella"},
-          {withdraw: "Ham"},
-          {withdraw: "Napoli"}
-        ]
+          { name: "Mozzarella", cost: 1 },
+          { name: "Ham", cost: 1 },
+          { name: "Napoli", cost: 1 },
+        ],
       },
     ],
   },
@@ -59,18 +59,18 @@ const menu = [
         ],
         image: "/images/pizza.jpg",
         additions: [
-          {add: "Tomato Sauce", cost: 2},
-          {add: "Mozarella", cost: 3},
-          {add: "Basil", cost: 2},
-          {add: "Ham", cost: 5},
-          {add: "Pepperoni", cost: 4},
-          {add: "Pineapple", cost: 4}
+          { name: "Tomato Sauce", cost: 2 },
+          { name: "Mozarella", cost: 3 },
+          { name: "Basil", cost: 2 },
+          { name: "Ham", cost: 5 },
+          { name: "Pepperoni", cost: 4 },
+          { name: "Pineapple", cost: 4 },
         ],
         remove: [
-          {withdraw: "Mozzarella"},
-          {withdraw: "Tomato Sauce"},
-          {withdraw: "Basil"}
-        ]
+          { name: "Mozzarella", cost: 1 },
+          { name: "Tomato Sauce", cost: 1 },
+          { name: "Basil", cost: 1 },
+        ],
       },
       {
         name: "Pepperoni",
@@ -82,18 +82,18 @@ const menu = [
         ],
         image: "/images/pizza.jpg",
         additions: [
-          {add: "Tomato Sauce", cost: 2},
-          {add: "Mozarella", cost: 3},
-          {add: "Basil", cost: 2},
-          {add: "Ham", cost: 5},
-          {add: "Pepperoni", cost: 4},
-          {add: "Pineapple", cost: 4}
+          { name: "Tomato Sauce", cost: 2 },
+          { name: "Mozarella", cost: 3 },
+          { name: "Basil", cost: 2 },
+          { name: "Ham", cost: 5 },
+          { name: "Pepperoni", cost: 4 },
+          { name: "Pineapple", cost: 4 },
         ],
         remove: [
-          {withdraw: "Mozzarella"},
-          {withdraw: "Tomato Sauce"},
-          {withdraw: "Pepperoni"}
-        ]
+          { name: "Mozzarella", cost: 1 },
+          { name: "Tomato Sauce", cost: 1 },
+          { name: "Pepperoni", cost: 1 },
+        ],
       },
       {
         name: "Vegetarian",
@@ -105,25 +105,25 @@ const menu = [
         ],
         image: "/images/pizza.jpg",
         additions: [
-          {add: "Tomato Sauce", cost: 2},
-          {add: "Mozarella", cost: 3},
-          {add: "Basil", cost: 2},
-          {add: "Ham", cost: 5},
-          {add: "Pepperoni", cost: 4},
-          {add: "Pineapple", cost: 4},
-          {add: "Mushroom", cost: 4},
-          {add: "Capsicum", cost: 2},
-          {add: "Olives", cost: 4},
-          {add: "Onion", cost: 2}
+          { name: "Tomato Sauce", cost: 2 },
+          { name: "Mozarella", cost: 3 },
+          { name: "Basil", cost: 2 },
+          { name: "Ham", cost: 5 },
+          { name: "Pepperoni", cost: 4 },
+          { name: "Pineapple", cost: 4 },
+          { name: "Mushroom", cost: 4 },
+          { name: "Capsicum", cost: 2 },
+          { name: "Olives", cost: 4 },
+          { name: "Onion", cost: 2 },
         ],
         remove: [
-          {withdraw: "Mozzarella"},
-          {withdraw: "Tomato Sauce"},
-          {withdraw: "Mushrooms"},
-          {withdraw: "Capsicum"},
-          {withdraw: "Olives"},
-          {withdraw: "Onion"}
-        ]
+          { name: "Mozzarella", cost: 1 },
+          { name: "Tomato Sauce", cost: 1 },
+          { name: "Mushrooms", cost: 1 },
+          { name: "Capsicum", cost: 1 },
+          { name: "Olives", cost: 1 },
+          { name: "Onion", cost: 1 },
+        ],
       },
       {
         name: "BBQ Chicken",
@@ -135,27 +135,27 @@ const menu = [
         ],
         image: "/images/pizza.jpg",
         additions: [
-          {add: "Tomato Sauce", cost: 2},
-          {add: "Mozarella", cost: 3},
-          {add: "Basil", cost: 2},
-          {add: "Ham", cost: 5},
-          {add: "Pepperoni", cost: 4},
-          {add: "Pineapple", cost: 4},
-          {add: "Mushroom", cost: 4},
-          {add: "Capsicum", cost: 2},
-          {add: "Olives", cost: 4},
-          {add: "Onion", cost: 2},
-          {add: "Grilled Chicken", cost: 6},
-          {add: "Red Onion", cost: 2},
-          {add: "BBQ Sauce", cost: 2}
+          { name: "Tomato Sauce", cost: 2 },
+          { name: "Mozarella", cost: 3 },
+          { name: "Basil", cost: 2 },
+          { name: "Ham", cost: 5 },
+          { name: "Pepperoni", cost: 4 },
+          { name: "Pineapple", cost: 4 },
+          { name: "Mushroom", cost: 4 },
+          { name: "Capsicum", cost: 2 },
+          { name: "Olives", cost: 4 },
+          { name: "Onion", cost: 2 },
+          { name: "Grilled Chicken", cost: 6 },
+          { name: "Red Onion", cost: 2 },
+          { name: "BBQ Sauce", cost: 2 },
         ],
         remove: [
-          {withdraw: "Mozzarella"},
-          {withdraw: "BBQ Sauce"},
-          {withdraw: "Grilled Chicken"},
-          {withdraw: "Red Onions"},
-          {withdraw: "Capsicum"}
-        ]
+          { name: "Mozzarella", cost: 1 },
+          { name: "BBQ Sauce", cost: 1 },
+          { name: "Grilled Chicken", cost: 2 },
+          { name: "Red Onions", cost: 1 },
+          { name: "Capsicum", cost: 1 },
+        ],
       },
       {
         name: "Meat Lovers",
@@ -167,28 +167,30 @@ const menu = [
         ],
         image: "/images/pizza.jpg",
         additions: [
-          {add: "Tomato Sauce", cost: 2},
-          {add: "Mozarella", cost: 3},
-          {add: "Basil", cost: 2},
-          {add: "Ham", cost: 5},
-          {add: "Pepperoni", cost: 4},
-          {add: "Pineapple", cost: 4},
-          {add: "Mushroom", cost: 4},
-          {add: "Capsicum", cost: 2},
-          {add: "Olives", cost: 4},
-          {add: "Onion", cost: 2},
-          {add: "Grilled Chicken", cost: 6},
-          {add: "Red Onion", cost: 2},
-          {add: "BBQ Sauce", cost: 2}
+          { name: "Tomato Sauce", cost: 2 },
+          { name: "Mozarella", cost: 3 },
+          { name: "Basil", cost: 2 },
+          { name: "Ham", cost: 5 },
+          { name: "Pepperoni", cost: 4 },
+          { name: "Pineapple", cost: 4 },
+          { name: "Mushroom", cost: 4 },
+          { name: "Capsicum", cost: 2 },
+          { name: "Olives", cost: 4 },
+          { name: "Onion", cost: 2 },
+          { name: "Grilled Chicken", cost: 6 },
+          { name: "Red Onion", cost: 2 },
+          { name: "BBQ Sauce", cost: 2 },
+          { name: "Bacon", cost: 2 },
+          { name: "Beef", cost: 3 },
         ],
         remove: [
-          {withdraw: "Mozzarella"},
-          {withdraw: "Tomato Sauce"},
-          {withdraw: "Pepperoni"},
-          {withdraw: "Ham"},
-          {withdraw: "Bacon"},
-          {withdraw: "Beef"}
-        ]
+          { name: "Mozzarella", cost: 1 },
+          { name: "Tomato Sauce", cost: 1 },
+          { name: "Pepperoni", cost: 2 },
+          { name: "Ham", cost: 2 },
+          { name: "Bacon", cost: 2 },
+          { name: "Beef", cost: 2 },
+        ],
       },
     ],
   },
@@ -200,21 +202,21 @@ const menu = [
         description:
           "Coffee-soaked sponge layered with mascarpone cream and cocoa.",
         cost: 8,
-        image: "/images/dessert.jpg"
+        image: "/images/dessert.jpg",
       },
       {
         name: "Chocolate Lava Cake",
         description:
           "Warm chocolate cake with a gooey molten center, served with cream.",
         cost: 9,
-        image: "/images/dessert.jpg"
+        image: "/images/dessert.jpg",
       },
       {
         name: "Gelato (2 scoops)",
         description:
           "Choice of flavors: vanilla, chocolate, strawberry, or pistachio.",
         cost: 7,
-        image: "/images/dessert.jpg"
+        image: "/images/dessert.jpg",
       },
     ],
   },
@@ -225,42 +227,46 @@ const menu = [
         name: "Garlic Bread",
         description: "Toasted baguette slices with garlic butter and parsley.",
         cost: 6,
-        image: "/images/sides.jpg"
+        image: "/images/sides.jpg",
       },
       {
         name: "Cheesy Garlic Bread",
         description:
           "Golden garlic bread topped with melted mozzarella cheese.",
         cost: 7,
-        image: "/images/sides.jpg"
+        image: "/images/sides.jpg",
       },
       {
         name: "Garden Salad",
         description: "Mixed greens, cucumber, tomato, and Italian dressing.",
         cost: 5.5,
-        image: "/images/sides.jpg"
+        image: "/images/sides.jpg",
       },
     ],
   },
-{
+  {
     category: "drinks",
     items: [
       {
-        name: "Coca Cola 1.25L", cost: 5,
-        image: ""
+        name: "Coca Cola 1.25L",
+        cost: 5,
+        image: "",
       },
       {
-        name: "Sprite 1.25L", cost: 5,
-        image: ""
+        name: "Sprite 1.25L",
+        cost: 5,
+        image: "",
       },
       {
-        name: "Fanta 1.25L", cost: 5,
-        image: ""
+        name: "Fanta 1.25L",
+        cost: 5,
+        image: "",
       },
       {
-        name: "Water 600ml", cost: 3.50,
-        image: ""
-      }
+        name: "Water 600ml",
+        cost: 3.5,
+        image: "",
+      },
     ],
   },
   {
@@ -273,10 +279,10 @@ const menu = [
         cost: 45,
         image: "/images/special.jpg",
         includes: {
-          pizza: { count: 2, size: "Large"},
+          pizza: { count: 2, size: "Large" },
           sides: 2,
-          drinks: 1
-        }
+          drinks: 1,
+        },
       },
       {
         name: "Couples Deal",
@@ -284,10 +290,10 @@ const menu = [
         cost: 30,
         image: "/images/special.jpg",
         includes: {
-          pizza: { count: 1, size: "Large"},
+          pizza: { count: 1, size: "Large" },
           sides: 1,
-          dessert: 1
-        }
+          dessert: 1,
+        },
       },
     ],
   },
